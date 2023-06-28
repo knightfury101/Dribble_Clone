a204911b0f6769c3e1e1d0fa0245c1bc7d64394a
Aditya Singh <adityasrajput786@gmail.com>
2023-06-28 03:48:42 -0400

    first commit

---
+++ components/AuthProviders.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AuthProviders = () => {
+  return (
+    <div>
+      auth
+    </div>
+  )
+}
+
+export default AuthProviders
---
+++ constants/index.ts
@@ -0,0 +1,96 @@
+export const NavLinks = [
+    { href: '/', key: 'Inspiration', text: 'Inspiration' },
+    { href: '/', key: 'Find Projects', text: 'Find Projects' },
+    { href: '/', key: 'Learn Development', text: 'Learn Development' },
+    { href: '/', key: 'Career Advancement', text: 'Career Advancement' },
+    { href: '/', key: 'Hire Developers', text: 'Hire Developers' }
+  ];
+  
+  export const categoryFilters = [
+    "Frontend",
+    "Backend",
+    "Full-Stack",
+    "Mobile",
+    "UI/UX",
+    "Game Dev",
+    "DevOps",
+    "Data Science",
+    "Machine Learning",
+    "Cybersecurity",
+    "Blockchain",
+    "E-Commerce",
+    "Chatbots"
+  ]
+  
+  export const footerLinks = [
+    {
+      title: 'For Developers',
+      links: [
+        'Go Pro!',
+        'Explore Development Work',
+        'Development Blog',
+        'Code Podcast',
+        'Open-Source Projects',
+        'Refer A Friend',
+        'Code Of Conduct',
+      ],
+    },
+    {
+      title: 'Hire Developers',
+      links: [
+        'Post A Job Opening',
+        'Post A Freelance Project',
+        'Search For Developers',
+      ],
+    },
+    {
+      title: 'Brands',
+      links: [
+        'Advertise With Us',
+      ],
+    },
+    {
+      title: 'Company',
+      links: [
+        'About',
+        'Careers',
+        'Support',
+        'Media Kit',
+        'Testimonials',
+        'API',
+        'Terms Of Service',
+        'Privacy Policy',
+        'Cookie Policy',
+      ],
+    },
+    {
+      title: 'Directories',
+      links: [
+        'Development Jobs',
+        'Developers For Hire',
+        'Freelance Developers For Hire',
+        'Tags',
+        'Places',
+      ],
+    },
+    {
+      title: 'Development Assets',
+      links: [
+        'Code Marketplace',
+        'GitHub Marketplace',
+        'NPM Registry',
+        'Packagephobia',
+      ],
+    },
+    {
+      title: 'Development Resources',
+      links: [
+        'Freelancing',
+        'Development Hiring',
+        'Development Portfolio',
+        'Development Education',
+        'Creative Process',
+        'Development Industry Trends',
+      ],
+    },
+  ];
